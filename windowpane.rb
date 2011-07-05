7e54cc8ab6c2aafb802739d46ca1d0cab67adc6e
Cody Brocious <cody.brocious@gmail.com>
2011-07-04 21:13:20 -0400

    Program and shader name remapping.

--- windowpane.rb
+++ windowpane.rb
@@ -27,6 +27,25 @@ def shadermin(shader)
 	shader
 end
 
+$i = 0
+$alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+$alphanum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789'
+def createIdentifier
+	ident = ''
+	$i += 1
+	ti = $i
+	while ti > 0
+		if ident.size == 0
+			ident += $alpha[ti % $alpha.size]
+			ti /= $alpha.size
+		else
+			ident += $alphanum[ti % $alphanum.size]
+			ti /= $alphanum.size
+		end
+	end
+	'$' + ident
+end
+
 $defaultTransform = %q{
 	attribute vec2 pos;
 	varying vec2 p;
@@ -41,29 +60,48 @@ def build(fn)
 	file = File.read(fn)
 	
 	title = /<title>(.*?)<\/title>/m.match(file) { |m| m[1].strip } || fn
-	fshaders = {}
+	tfshaders = {}
 	file.scan(/<fshader\s(.*?)>(.*?)<\/fshader>/m) do |m|
-		fshaders[m[0].strip] = "#ifdef GL_ES\nprecision highp float;\n#endif\n" + shadermin(m[1].strip)
+		tfshaders[m[0].strip] = "#ifdef GL_ES\nprecision highp float;\n#endif\n" + shadermin(m[1].strip)
 	end
-	vshaders = {}
+	tvshaders = {}
 	file.scan(/<vshader\s(.*?)>(.*?)<\/vshader>/m) do |m|
-		vshaders[m[0].strip] = shadermin(m[1].strip)
+		tvshaders[m[0].strip] = shadermin(m[1].strip)
 	end
-	programs = {}
+	tprograms = {}
 	file.scan(/<program\s*?(\s.+?)?>(.*?)<\/program>/m) do |m|
-		programs[(m[0] || 'main').strip] = m[1].strip.split ' '
+		tprograms[(m[0] || 'main').strip] = m[1].strip.split ' '
 	end
 	update = /<update>(.*?)<\/update>/m.match(file) { |m| m[1].strip } || nil
 	drawMulti = (file =~ /<singleframe>/) == nil
 	
-	programs.each do |k, v|
+	tprograms.each do |k, v|
 		next if v.size != 1
-		vshaders['defaultTransform'] ||= $defaultTransform
-		programs[k] << 'defaultTransform'
+		tvshaders['defaultTransform'] ||= $defaultTransform
+		tprograms[k] << 'defaultTransform'
+	end
+	
+	remap = {}
+	programs = {}
+	tprograms.each do |k, v|
+		remap[k] = createIdentifier
+		programs[remap[k]] = v.map do |x|
+			remap[x] = createIdentifier if not remap.include? x
+			remap[x]
+		end
+	end
+	
+	vshaders = {}
+	tvshaders.each do |k, v|
+		vshaders[remap[k]] = v if remap.include? k
+	end
+	fshaders = {}
+	tfshaders.each do |k, v|
+		fshaders[remap[k]] = v if remap.include? k
 	end
 	
 	script = ERB.new(File.read('script.jst')).result(binding)
-	script = JSMin.minify script
+	#script = JSMin.minify script
 	
 	doc = %Q{<!doctype html><title>#{title}</title><script>#{script}</script><body onload="r()" style="width:100%;height:100%;border:0px;padding:0px;margin:0px;overflow:hidden"><canvas id="c" style="width:100%;height:100%">}
 	puts "Size: #{doc.size} bytes"
